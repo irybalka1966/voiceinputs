9160c6c13b27663a2205bdf87465031746593cac
irybalka1966 <irybalka@yahoo.com>
2019-09-03 00:15:24 +0300

    Create VoiceInputsSpec.php

---
+++ spec/VoiceInputsSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace spec\irybalka1966\VoiceInputs;
+
+use PhpSpec\ObjectBehavior;
+use irybalka1966\VoiceInputs\VoiceInputs;
+
+class VoiceInputsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(VoiceInputs::class);
+    }
+
+    function it_can_text()
+    {
+        $this->TextVoice('test name')->shouldReturn('test name');
+    }
+
+    function it_can_select(){
+        $this->TextSelect('test name')->shouldReturn('test name');
+    }
+
+    
+}
